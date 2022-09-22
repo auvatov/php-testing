5c97058bb15bffa48fbd8946a9094b38fd724e1b
Andrey Uvatov <auvatov@gmail.com>
2022-09-22 09:41:51 +0800

    add webmozart/assert in tests

--- tests/StringUtilsTest.php
+++ tests/StringUtilsTest.php
@@ -1,13 +1,14 @@
 <?php
 
 require_once __DIR__ . '/../src/StringUtils.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-if (StringUtils\capitalize('hello') !== 'Hello') {
-    throw new \Exception('Функция работает неверно!');
-}
+use Webmozart\Assert\Assert;
+use function StringUtils\capitalize;
 
-if (StringUtils\capitalize('') !== '') {
-    throw new \Exception('Функция работает неверно!');
-}
-
-echo 'Все тесты пройдены!';
+// eq означает equal
+Assert::eq(capitalize(''), '');
+// Первый параметр actual – то, что пришло
+// Второй параметр expected – то, что ожидает тест
+// Правильный порядок аргументов имеет большое значение при анализе ошибки
+Assert::eq(capitalize('hello'), 'Hello');
